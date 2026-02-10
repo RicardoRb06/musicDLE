3cef3ce13ca019f6338b9f68bc3b32e5c11b674a
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-10 17:37:29 -0300

    first commit

---
+++ src/main/java/com/example/musicDLE/MusicDleApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicDLE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDleApplication.class, args);
+	}
+
+}
