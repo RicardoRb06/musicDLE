1ac39b56e12064164e3f12816a3c002182b5bd69
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-12 20:01:15 -0300

    create getters of artistsDto

--- src/main/java/com/example/musicDLE/dto/artistDto.java
+++ src/main/java/com/example/musicDLE/dto/artistDto.java
@@ -2,8 +2,23 @@ package com.example.musicDLE.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.net.URL;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class artistDto {
     private int id;
     private String name;
+    private URL picture;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public URL getPicture() {
+        return picture;
+    }
 }
