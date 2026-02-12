154c3726dd5ed82d0bf69891390b6bbb681d8365
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-12 18:12:17 -0300

    add jackson library and create artistDto

---
+++ src/main/java/com/example/musicDLE/dto/artistDto.java
@@ -0,0 +1,9 @@
+package com.example.musicDLE.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class artistDto {
+    private int id;
+    private String name;
+}
