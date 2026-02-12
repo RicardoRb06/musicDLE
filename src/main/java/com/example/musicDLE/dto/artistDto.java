fcd588dececc497ccd39522cdcc738dfa2631981
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-12 18:17:54 -0300

    fix the dto jackson anotation

--- src/main/java/com/example/musicDLE/dto/artistDto.java
+++ src/main/java/com/example/musicDLE/dto/artistDto.java
@@ -2,7 +2,7 @@ package com.example.musicDLE.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class artistDto {
     private int id;
     private String name;
