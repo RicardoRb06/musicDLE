6f9f1c1c16a2fc1a5acb68e8cf9fc1fd05428392
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-12 17:14:49 -0300

    create setArtist endpoint

--- src/main/java/com/example/musicDLE/controller/Controller.java
+++ src/main/java/com/example/musicDLE/controller/Controller.java
@@ -1,17 +1,16 @@
 package com.example.musicDLE.controller;
 
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Mapping("/api")
 public class Controller {
 
-    public record TextDTO(String text) {}
+    @PostMapping("/setArtist")
+    public String printTest(){
 
-    @PostMapping("/api")
-    public String printTest(@RequestBody TextDTO dto){
-        System.out.println(dto.text());
-        return "ok";
     }
 }
