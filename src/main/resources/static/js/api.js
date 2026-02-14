2aeb54b572aedeca6d307a17b4b0f90acd9b4743
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-14 02:43:22 -0300

    finish the searchbar

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,13 +1,34 @@
-async function submit(){
-    const text = document.getElementById(`text`).value;
-    const type = document.getElementById(`typeText`).value;
+const inputText = document.getElementById(`text`);
+const select = document.getElementById(`typeText`);
+let time
+
+inputText.addEventListener(`input`, async function() {
+    const text = inputText.value;
+    clearTimeout(time);
+    if(text.length > 2){
+        time = setTimeout(search, 500);
+    }else{
+        document.getElementById(`searchResults`).innerHTML = ``;
+    }
+});
+
+select.addEventListener(`change`, function(){
+    inputText.dispatchEvent(new Event(`input`));
+});
+
+
+async function search(){
+    const text = inputText.value;
+    const type = select.value;
     let url = `api/`;
 
+    
     switch (type) {
         case `artist`:
             url += `searchArtist` 
             break;
     }
+        
 
     const response = await fetch(url, {
         method: "POST",
@@ -17,9 +38,11 @@ async function submit(){
         body: JSON.stringify({
             text: text
         })
-    }).then(res => res.json());
+    });
+    await response.json();
     console.log(response)
 
+    if(text !== inputText.value) return;
     if(response.status === `FOUND`){
         const searchList = document.getElementById(`searchResults`);
         let lista = ``;
@@ -27,7 +50,7 @@ async function submit(){
         response.results.forEach(artist => {
             lista += 
                     `<li>
-                        <button>
+                        <button value="${artist.id}">
                             <img src="${artist.picture}">
                             ${artist.name}
                         </button>
@@ -35,8 +58,7 @@ async function submit(){
         });
 
         searchList.innerHTML = lista;
+        return;
     }
-    if(response.status === `NOT_FOUND`){
-        const searchList = document.getElementById(`searchResults`).innerHTML = `Nenhum resultado encontrado`;
-    }
+    document.getElementById(`searchResults`).innerHTML = `Nenhum resultado encontrado`;
 }
