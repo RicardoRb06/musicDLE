4112170a3f18566d0527dae0e1ea28991d02a43c
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-20 20:58:32 -0300

    create nb_fan and picture xl

--- src/main/java/com/example/musicDLE/dto/API/ArtistDto.java
+++ src/main/java/com/example/musicDLE/dto/API/ArtistDto.java
@@ -6,10 +6,19 @@ import java.net.URL;
 
 public class ArtistDto {
     private int id;
+    private int nb_fan;
     private String name;
     private URL picture;
+    private URL picture_xl;
 
 
+    public int getNb_fan() {
+        return nb_fan;
+    }
+
+    public URL getPicture_xl() {
+        return picture_xl;
+    }
 
     public int getId() {
         return id;
